35172b60663244bee4c874909d89df99ddf7eb74
simran <simranbaruwal24@gmail.com>
2021-12-02 20:48:04 +0000

    .

--- R/comparision.R
+++ R/comparision.R
@@ -8,7 +8,11 @@
 #'
 #'
 ants <- read_tsv("data/ants_timevary.log")
-simulated_data <- trees$root.edge[[1]]
+i = 1
+for (i in 1:20) {
+  simulated_data <- trees$root.edge[[i]]
+}
+
 emperical_data <- ants$origin_time
 mean <- mean(emperical_data)
 ggplot(data.frame(x= simulated_data), aes(simulated_data)) + geom_density() +geom_vline(xintercept = mean, linetype = "dotted", col = 2)

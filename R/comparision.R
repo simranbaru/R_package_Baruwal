fb95d830da3996704398815dd8205523461ab190
simran <simranbaruwal24@gmail.com>
2021-12-02 20:38:14 +0000

    code corrected

--- R/comparision.R
+++ R/comparision.R
@@ -11,5 +11,6 @@
 i = 1
 for (i in 1:20)
   simulated_data[i] <- trees[[i]]$root.edge
+
 mean <- mean(emperical_data)
 ggplot(data.frame(x= simulated_data), aes(simulated_data)) + geom_density() +geom_vline(xintercept = mean, linetype = "dotted", col = 2)

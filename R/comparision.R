082e7c3b9f85f54b5a1095b5168c9e84e57b6386
simran <simranbaruwal24@gmail.com>
2021-12-02 20:42:15 +0000

    added

--- R/comparision.R
+++ R/comparision.R
@@ -9,5 +9,6 @@
 #'
  
 simulated_data <- trees$root.edge[[1]]
+emperical_data <- ants$origin_time
 mean <- mean(emperical_data)
 ggplot(data.frame(x= simulated_data), aes(simulated_data)) + geom_density() +geom_vline(xintercept = mean, linetype = "dotted", col = 2)

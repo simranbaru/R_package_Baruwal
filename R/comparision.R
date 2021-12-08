9721a7e71422cf0e40761cea5dc2708a4d945e6d
simran <simranbaruwal24@gmail.com>
2021-12-08 17:04:02 +0000

    .

--- R/comparision.R
+++ R/comparision.R
@@ -1,7 +1,7 @@
 #' Comparision of simulated data with emperical data
 #'
 #' @param datasdf default dataset to use for plot.
-#' @return q
+#' @return p
 #' @export
 #' 
 #' @usage 
@@ -10,8 +10,7 @@
 
 
 comparision<- function(datasdf){
-p <- ggplot2::ggplot(datasdf, aes(fill = simulated, x = simulated)) +  ggplot2::geom_density()
-q <- p +ggplot2::geom_vline(aes(xintercept = mean, linetype = "dotted", col = 2))
-return(q)
-print(q)
+p <- ggplot2::ggplot(datasdf, aes(fill = simulated, x = simulated)) +  ggplot2::geom_density() +ggplot2::geom_vline(aes(xintercept = mean, linetype = "dotted", col = 2))
+return(p)
+print(p)
 }

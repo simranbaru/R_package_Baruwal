69d08c819861201edafb1c4132745fa73be7c6e2
simran <simranbaruwal24@gmail.com>
2021-12-08 22:14:57 +0000

    new function added

---
+++ R/effect_size.R
@@ -0,0 +1,14 @@
+#' Calculation of effect size to know how many standard deviation away from the median is the observed value.
+#'
+#'@param datasdf default data frame which contains the data
+#'@return eff_size
+#'@export
+#'@usage
+#'effect_size(datasdf = datas)
+#'
+
+effect_size <- function(datasdf) {
+  d = abs((mean- means)/((((sd(emperical_data))^2 + (sd(simulated))^2)/2)^(1/2)))
+  eff_size <- (d/((d^2+4))^(1/2))
+  return(eff_size)
+}

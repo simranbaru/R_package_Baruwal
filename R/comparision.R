92b10d6827207cac14ab4678f99b21026148ae21
simran <simranbaruwal24@gmail.com>
2021-12-08 18:38:15 +0000

    .

--- R/comparision.R
+++ R/comparision.R
@@ -13,4 +13,4 @@ comparision<- function(datasdf){
 p <- ggplot2::ggplot(datasdf, aes(fill = simulated, x = simulated)) +  ggplot2::geom_density() +ggplot2::geom_vline(aes(xintercept = mean, linetype = "dotted", col = 2))
 return(p)
 print(p)
-}
+} 
